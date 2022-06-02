373a9349cfc220abbff717b1ddab6a4a2038bf6c
alaa <ahmed.abourizk@capgemini.com>
2022-06-02 10:16:45 +0200

    adding a MUI theme

---
+++ src/theme/index.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles'
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#eb6f25',
+      contrastText: 'white',
+    },
+  },
+})
+
+export default customTheme
