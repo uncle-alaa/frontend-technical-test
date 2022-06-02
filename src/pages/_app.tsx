50db734adeaf47746f5fe18a41db1e01653c19d3
alaa <ahmed.abourizk@capgemini.com>
2022-06-02 10:19:40 +0200

    adding a Context with logged user info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 import type { AppProps } from 'next/app'
 import { getLoggedUserId } from '../utils/getLoggedUserId'
 import '../styles/globals.css'
+import { ThemeProvider } from '@mui/system'
+import customTheme from '../theme'
+import { UserContext } from './context'
+import useSWR from 'swr'
 
-// Default way to get a logged user
-export const loggedUserId = getLoggedUserId()
+const loggedUserId = getLoggedUserId()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const fetcher = (url) => fetch(url).then((res) => res.json())
+  const { data } = useSWR(`http://localhost:3005/user/${loggedUserId}`, fetcher)
+  const loggedUser = data ? data[0] : null
+  return (
+    <UserContext.Provider value={loggedUser}>
+      <ThemeProvider theme={customTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UserContext.Provider>
+  )
 }
 
 export default MyApp
